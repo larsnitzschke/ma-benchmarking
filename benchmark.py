abd730cab2156ff78faf3a7f4d8e2bd9957e1930
Lars Nitzschke <lars.nitzschke@tu-dortmund.de>
2025-11-18 17:15:04 +0100

    multiple runs

--- benchmark.py
+++ benchmark.py
@@ -65,38 +65,39 @@ with open(f"{path_to_examples}/examples-list.txt", "r") as file:
         tags = split[3].split(",") if len(split) > 3 else []
         examples.append(Example(f"{path_to_examples}/{split[0]}", split[1], bool(split[2]), tags))
 
-# Benchmarking: Warm-up, BMC; k-induction, WPC-Proof, GPDR, GPDR with boolean evaluation
-modes = ["--warmup", "-b", "-k", "-p", "-g", "-gB"]
+# Benchmarking: Warm-up, BMC, k-induction, BMC+k-ind, WPC-Proof, GPDR, GPDR with boolean evaluation, GPDR with ArrayTransitionSystems, GPDR with SubModelInterpolation
+modes = ["--warmup", "-b", "-k", "-bk", "-p", "-g", "-gB", "-g --gpdr-smi", "-gB --gpdr-smi", "-g --gpdr-ats", "-gB --gpdr-ats", "-g --gpdr-smi --gpdr-ats", "-gB --gpdr-smi --gpdr-ats"]
 limit_examples = None  # Set to an integer to limit number of examples for testing
 
 # Main Loop
 results = []
 for mode in modes:
     for example in examples[:limit_examples]:
-        print(f"Running benchmark for example: {example.name} with mode: {mode}")
-        try:
-            result = subprocess.run(["/usr/bin/time", "-v", wvm_gradle, "run", f"--args={mode} {example.path}"],
-                                capture_output=True, text=True,
-                                cwd=path_to_whilestar,
-                                timeout=10)
-        except subprocess.TimeoutExpired as e:
-            print(f"Timeout expired for example: {example.name} with mode: {mode}")
-            results.append([example.name, mode, "TIMEOUT", None, None, None, None, None, None, None, " ".join(example.tags)])
-            continue
+        for run_number in range(5):
+            print(f"Running benchmark for example: {example.name} with mode: {mode}")
+            try:
+                result = subprocess.run(["/usr/bin/time", "-v", wvm_gradle, "run", f"--args={mode} {example.path}"],
+                                    capture_output=True, text=True,
+                                    cwd=path_to_whilestar,
+                                    timeout=60)
+            except subprocess.TimeoutExpired as e:
+                print(f"Timeout expired for example: {example.name} with mode: {mode}")
+                results.append([example.name, mode, "TIMEOUT", None, None, None, None, None, None, None, " ".join(example.tags)])
+                continue
 
-        metrics = extract_metrics(result)
-        results.append([example.name, mode, 
-                        metrics["verification_result"], metrics["num_smt_calls"],
-                        metrics["user_time_sec"], metrics["system_time_sec"],
-                        metrics["cpu_percent"], metrics["elapsed_time"],
-                        metrics["max_memory_kb"],
-                        classification(metrics["verification_result"], example.expected_safety),
-                        " ".join(example.tags)])
+            metrics = extract_metrics(result)
+            results.append([example.name, mode, run_number,
+                            metrics["verification_result"], metrics["num_smt_calls"],
+                            metrics["user_time_sec"], metrics["system_time_sec"],
+                            metrics["cpu_percent"], metrics["elapsed_time"],
+                            metrics["max_memory_kb"],
+                            classification(metrics["verification_result"], example.expected_safety),
+                            " ".join(example.tags)])
 
 csv_filename = f"benchmark_results_{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
 with open(csv_filename, "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Example", "Mode", "Safe", "NumberOfSMTCalls",
+    writer.writerow(["Example", "Run Number", "Mode", "Safe", "NumberOfSMTCalls",
                      "UserTimeSec", "SystemTimeSec", "CPUPercent",
                      "ElapsedTime", "MaxMemoryKB", "Classification", "Tags"])
     writer.writerows(results)
